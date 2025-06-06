b040606a1a3b73244a68e35740fb52bfb943eeab
AthaTajuddin <mochathatajuddin02@gmail.com>
2025-06-06 14:51:38 +0700

    Update latihan

--- rust1_2.rs
+++ rust1_2.rs
@@ -6,5 +6,13 @@ fn main(){
     let a: i32 = 10;
     let b: f64 = 22.5;
     let result = a as f64 +b;
+    let string = String::from("Halo ");
+    let s:String = "World".to_string();
+    let gabungkan_string = string.clone() + &s;
+    println!("Gabungan string {}",gabungkan_string);
     println!("Hasil penjumlahan: {}", result);
+    println!("{}", string);
+
+
+    // metode lain let gabungkan_string = &string + s.as_str(); 
 }
