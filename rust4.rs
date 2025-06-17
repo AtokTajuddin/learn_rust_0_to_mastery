3da278ff20e170bc6021fa49bae7904786cc940a
AthaTajuddin <mochathatajuddin03@gmail.com>
2025-06-17 21:32:34 +0700

    Latihan day 4

---
+++ rust4.rs
@@ -0,0 +1,15 @@
+// fn main(){
+//     let spasi = "Ini adalah contoh string dengan spasi";
+//     let spasi= spasi.len();
+//     println!("Terdeteksi spasi {} ",spasi);
+// }
+fn hitung_spasi(text:&str) -> usize{
+    text.chars().filter(|c| c.is_whitespace()).count()
+}
+
+fn main(){
+    let text = "Halo ini uji coba program fungsi pendeteksi spasi";
+    let hitung_spasi_text = hitung_spasi(text);
+    println!("Teks nya {} ",text);
+    println!("Jumlah spasinya : {} ",hitung_spasi_text);
+}
