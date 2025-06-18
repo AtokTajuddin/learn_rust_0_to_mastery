9347027059708ef35fb595e68985f6e26bb25642
AthaTajuddin <mochathatajuddin03@gmail.com>
2025-06-18 14:03:23 +0700

    Latihan day 5 array & tuple input

--- rust5_3.rs
+++ rust5_3.rs
@@ -1,10 +1,14 @@
+use std::io;
+
 fn main(){
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error input yang anda masukkan salah");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
     let contoh_tuple: Vec<String> = input
-    .trim()
-    .split_whitespace()
-    .map(|s|s.parse().expect("Inputan salah"))
-    .collect()
+        .trim()
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect();
+    
     println!("Nama: {}, Umur: {}", contoh_tuple[0], contoh_tuple[1]);
 }
